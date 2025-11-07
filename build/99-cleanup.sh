7102bd2d6dc584e6307310bdb816dbaa7d2c5e17
KiKaraage <kikaraage@tuta.io>
2025-11-07 16:24:02 +0700

    fix(build): reduce redundant remove calls

--- build/99-cleanup.sh
+++ build/99-cleanup.sh
@@ -8,34 +8,7 @@ set -eoux pipefail
 # This script removes GNOME/Silverblue packages that are not needed in base-main
 ###############################################################################
 
-echo "::group:: Remove GNOME Software Packages"
-
-echo "Removing GNOME software packages..."
-dnf5 remove -y \
-    gnome-software \
-    gnome-software-rpm-ostree \
-    firefox \
-    firefox-langpacks \
-    podman-docker \
-    yelp || true
-
-echo "GNOME software packages removed"
-echo "::endgroup::"
-
-echo "::group:: Remove GNOME Integration Packages"
-
-echo "Removing GNOME integration packages..."
-dnf5 remove -y \
-    totem-video-thumbnailer \
-    gnome-terminal-nautilus \
-    fedora-bookmarks \
-    fedora-chromium-config \
-    fedora-chromium-config-gnome \
-    gnome-extension-app \
-    gnome-shell-extension-background-logo || true
-
-echo "GNOME integration packages removed"
-echo "::endgroup::"
+echo "::group:: Clean Up Package Cache"
 
 echo "::group:: Clean Up Package Cache"
 
