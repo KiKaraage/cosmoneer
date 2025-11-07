5ba92319d270f8e681755575ed02e0e296f664fe
KiKaraage <kikaraage@tuta.io>
2025-11-07 14:08:31 +0700

    fix: add missing UBLUE_IMAGE_TAG variable to 15-image-id.sh

--- build/15-image-id.sh
+++ build/15-image-id.sh
@@ -14,6 +14,7 @@ SUPPORT_URL="https://github.com/KiKaraage/Cosmoneer/issues"
 BUG_SUPPORT_URL="https://github.com/KiKaraage/Cosmoneer/issues"
 CODE_NAME="Alpha"
 VERSION="${VERSION:-43.$(date +%y%m%d).0}"
+UBLUE_IMAGE_TAG="${UBLUE_IMAGE_TAG:-daily}"
 
 IMAGE_INFO="/usr/share/ublue-os/image-info.json"
 IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
