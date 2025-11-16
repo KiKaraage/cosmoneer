fe16d0cd6f6ff928a56dd9cc921caed05515c6a0
KiKaraage <kikaraage@tuta.io>
2025-11-16 20:13:01 +0700

    feat(service): try COSMIC;GNOME for portal service
    - Remove `Environment=DISPLAY=` in xdg-desktop-portal-gtk service
    - Use COSMIC first, than GNOME as fallback
    - Remove GTK as the portal used for file chooser (for now, uses
    cosmic;gnome)
    
    If this fails, then I will just use gnome portal for filechooser

--- build/24-niri-services.sh
+++ build/24-niri-services.sh
@@ -10,11 +10,5 @@ set -eoux pipefail
 
 echo "::group:: Configure Portal Services"
 
-# Ensure GTK portal is enabled for Niri file chooser dialogs
-mkdir -p "/etc/systemd/user/graphical-session.target.wants"
-if [ -f "/usr/lib/systemd/user/xdg-desktop-portal-gtk.service" ]; then
-    ln -sf "/usr/lib/systemd/user/xdg-desktop-portal-gtk.service" \
-        "/etc/systemd/user/graphical-session.target.wants/xdg-desktop-portal-gtk.service"
-fi
 
 echo "::endgroup::"
